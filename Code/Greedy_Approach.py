4bbf4841aea6d0425ca7c9320e0c981a9a221fb5
Rafael23082 <144778682+Rafael23082@users.noreply.github.com>
2025-01-08 11:11:55 +0700

    Update Greedy_Approach.py

--- Code/Greedy_Approach.py
+++ Code/Greedy_Approach.py
@@ -1,3 +1,6 @@
+import time
+import tracemalloc
+
 def calculate_cost(arrangement, distance_matrix, flow_matrix):
     total_cost = 0
     i_count = 0
@@ -24,30 +27,30 @@ def get_row_sum(matrix):
     return container
 
 def greedy_approach(distance_matrix, flow_matrix):
+    start = time.perf_counter()
+    tracemalloc.start()
+
     distance_list = get_row_sum(distance_matrix)
     flow_list = get_row_sum(flow_matrix)
 
     distance = sorted([i for i in range(len(distance_list))], key=lambda i: distance_list[i], reverse=True) # Contains the index, but sorted by the sum of the array
-    flow = sorted([i for i in range(len(flow_list))], key=lambda i: flow_list[i])
+    flow = sorted([i for i in range(len(flow_list))], key=lambda i: flow_list[i]) # Each value of the input array is assigned with a new value, and the original values are sorted by that new value.
 
-    arrangement = []
-    for i in range(len(distance)):
-        index = distance.index(i)
-        arrangement.append(flow[index] + 1)
+    arrangement = [] # Flow[i] is assigned to Distance[i]
+    for i in range(len(distance)): # i or index of the arrangement list represents the location.
+        index = distance.index(i) # Find index of location i 
+        arrangement.append(flow[index] + 1) # For location i, append the flow assigned to it, and add it by 1 (Because the calculate cost function calculates for workstations 1 and so on)
 
     optimal_cost = calculate_cost(arrangement, distance_matrix, flow_matrix)
     print(f"Optimal arrangement: {arrangement}, cost: {optimal_cost}")
 
-distance_matrix = [
-    [0, 3, 5],
-    [3, 0, 4],
-    [5, 4, 0],
-]
- 
-flow_matrix = [
-    [0, 2, 1],
-    [2, 0, 3],
-    [1, 3, 0],
-]
+    end = time.perf_counter()
+    exec_time = end - start
+    print(f"Execution Time: {exec_time}")
+    print(tracemalloc.get_traced_memory())
 
+    tracemalloc.stop()
+
+distance_matrix = [[0, 2, 6, 6, 1, 4, 3, 9, 7, 6], [2, 0, 7, 8, 5, 6, 4, 6, 2, 4], [6, 7, 0, 10, 4, 10, 2, 9, 7, 5], [6, 8, 10, 0, 6, 7, 7, 4, 5, 3], [1, 5, 4, 6, 0, 3, 10, 2, 1, 10], [4, 6, 10, 7, 3, 0, 9, 10, 6, 8], [3, 4, 2, 7, 10, 9, 0, 5, 8, 7], [9, 6, 9, 4, 2, 10, 5, 0, 9, 3], [7, 2, 7, 5, 1, 6, 8, 9, 0, 7], [6, 4, 5, 3, 10, 8, 7, 3, 7, 0]]
+flow_matrix = [[0, 3, 3, 6, 5, 6, 9, 1, 9, 3], [3, 0, 6, 10, 2, 2, 10, 1, 3, 9], [3, 6, 0, 8, 8, 6, 8, 1, 8, 4], [6, 10, 8, 0, 6, 8, 7, 5, 3, 4], [5, 2, 8, 6, 0, 8, 9, 10, 9, 7], [6, 2, 6, 8, 8, 0, 5, 10, 10, 9], [9, 10, 8, 7, 9, 5, 0, 5, 8, 4], [1, 1, 1, 5, 10, 10, 5, 0, 7, 1], [9, 3, 8, 3, 9, 10, 8, 7, 0, 2], [3, 9, 4, 4, 7, 9, 4, 1, 2, 0]]
 greedy_approach(distance_matrix, flow_matrix)
